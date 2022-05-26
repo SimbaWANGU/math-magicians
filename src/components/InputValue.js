c9875c4ec59e17b958bd603df9d0f9507ee1b764
SimbaWANGU <simbawangulu@gmail.com>
2022-05-26 12:51:38 +0300

    Refactor to functional components

--- src/components/InputValue.js
+++ src/components/InputValue.js
@@ -1,27 +1,16 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable react/destructuring-assignment */
-
 import React from 'react';
 import './InputValue.css';
 import PropTypes from 'prop-types';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class InputValue extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const { handleClick } = this.props;
-    return (
-      <div className="inputValue" style={{ backgroundColor: this.props.backgroundColor }}>
-        <p className="nameOfValue" onClick={() => handleClick(this.props.content)}>{this.props.content}</p>
-      </div>
-    );
-  }
-}
+const InputValue = ({ content, backgroundColor, handleClick }) => {
+  (
+    <div className="inputValue" style={{ backgroundColor: { backgroundColor } }}>
+      <p className="nameOfValue" onClick={() => handleClick({ content })}>{content}</p>
+    </div>
+  );
+};
 
 InputValue.propTypes = {
   content: PropTypes.string.isRequired,
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,22 +1,18 @@
-/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import './Result.css';
 import PropTypes from 'prop-types';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class Result extends React.Component {
-  render() {
-    return (
-      <input
-        className="resultContainer"
-        value={this.props.result}
-        type="text"
-        onKeyDown={(e) => e.preventDefault()}
-        disabled
-      />
-    );
-  }
-}
+const Result = ({ result }) => {
+  (
+    <input
+      className="resultContainer"
+      value={result}
+      type="text"
+      onKeyDown={(e) => e.preventDefault()}
+      disabled
+    />
+  );
+};
 
 Result.propTypes = {
   result: PropTypes.number,
