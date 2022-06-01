c7f6e09f858bdfa2b394f364a08ec772265f8637
SimbaWANGU <simbawangulu@gmail.com>
2022-06-01 11:01:13 +0300

    Add navigation with react router

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="homeDiv">
+      <h1 className="homeTitle">Welcome to Math Magicians</h1>
+      <p className="homeParagraph">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        <br />
+        Architecto dignissimos rem, illum libero voluptatem enim
+        <br />
+        eaque ipsa optio officia fuga nulla omnis aut qui dolores
+        <br />
+        accusantium et consequatur minus. Fuga ad quis amet ipsam
+        <br />
+        vitae optio quod, nam error nulla reprehenderit nisi odit
+        <br />
+        ullam voluptas suscipit facilis, inventore saepe tempora?
+      </p>
+    </div>
+  );
+}
+
+export default Home;
